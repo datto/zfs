0a6cc7091a531a70817b47de23c68941967aff9f
Tom Caputi <tcaputi@datto.com>
2017-11-08 14:30:44 -0500

    Compress indirect blocks of encrypted metadnode
    Fixes a small issue where the indirect blocks of an
    encrypted objset's metadnode were not being compressed.
    
    Signed-off-by: Tom Caputi <tcaputi@datto.com>

--- module/zfs/dmu.c
+++ module/zfs/dmu.c
@@ -2207,8 +2207,10 @@ dmu_write_policy(objset_t *os, dnode_t *dn, int level, int wp, zio_prop_t *zp)
 			dedup = B_FALSE;
 		}
 
-		if (type == DMU_OT_DNODE || type == DMU_OT_OBJSET)
+		if (level <= 0 &&
+		    (type == DMU_OT_DNODE || type == DMU_OT_OBJSET)) {
 			compress = ZIO_COMPRESS_EMPTY;
+		}
 	}
 
 	zp->zp_compress = compress;
